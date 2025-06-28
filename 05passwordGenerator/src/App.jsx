8d74af07cd73335ac3a9767d6a582caf03103a9e
Utkarsh Singh <singhutkarsh9559@gmail.com>
2025-06-29 02:03:15 +0530

    react

---
+++ 05passwordGenerator/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState , useCallback } from 'react'
+
+import './App.css'
+
+function App() {
+  const [length, setLength] = useState(8)
+  const [numberAllowed , setnumberAllowed] = useState(false)
+  const [charAllowed , setcharAllowed] = useState(false)
+  const [Password , setPassword] = useState("")
+
+  const passwordGenerator = useCallback( () => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    
+    if(numberAllowed) str += "0123456789"
+    if(charAllowed) str += "!@#$%^&*()-_=+[]{}|;:',.<>?/~` "
+
+    for (let i = 1; i <= array.length; i++) {
+      let char = Math.floor(Math.random()* str.length + 1) 
+      pass = str.charAt(char)
+      
+    }
+    setPassword(pass)
+
+  } , [length , numberAllowed , charAllowed , setPassword] )
+
+  return (
+    <>
+   <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700'> Test</div>
+    </>
+  )
+}
+
+export default App
